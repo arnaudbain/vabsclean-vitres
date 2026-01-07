9b651e30838f14c10a928926c39536c8b6e9b6c9
arnaudbain <arnaudbain@gmail.com>
2026-01-07 18:47:33 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,3 @@
-
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Vab's Clean | Nettoyage Vitres & Solaire",
-  description: "Nettoyage professionnel à Mirambeau",
-};
-
 export default function RootLayout({
   children,
 }: {
