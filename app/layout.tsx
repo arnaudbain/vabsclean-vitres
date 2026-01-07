e7ce54da529a55c50c883e06953ca1a30bcaa9e9
arnaudbain <arnaudbain@gmail.com>
2026-01-07 18:26:43 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,22 @@
 
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Vab's Clean | Nettoyage Vitres & Solaire",
+  description: "Nettoyage professionnel à Mirambeau",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
